28f6ddef990cdfb28f232196000513373343ec04
VLN37 <54167882+VLN37@users.noreply.github.com>
2021-12-21 11:56:59 -0300

    organization: repository structure

---
+++ philo/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/20 08:48:45 by jofelipe          #+#    #+#             */
+/*   Updated: 2021/12/20 08:52:45 by jofelipe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+#endif
+
+# include <pthread.h>
+# include <string.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <sys/wait.h>
