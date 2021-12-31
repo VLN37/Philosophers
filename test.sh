0db43be469d4d66c07a8606d8055119983eeb272
VLN37 <jofelipe@student.42sp.org>
2021-12-31 13:16:21 +0100

    tests: script tweaks

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 x=1
 time=0.1
-echo 1 400 100 100 should die
+echo "1 400 100 100 should die\n"
 while [ $x -le 2 ]
 do
 	echo "Test: $x"
-	./bin/philo 4 210 100 100 7 | grep died
+	./bin/philo 1 400 100 100 7 | grep die
 	sleep $time
 	x=$(( $x + 1 ))
 done
@@ -12,44 +12,66 @@ done
 
 x=1
 time=0.1
-echo 4 210 100 100 not die
+echo "\n4 210 100 100 not die\n"
 while [ $x -le 25 ]
 do
 	echo "Test: $x"
-	./bin/philo 4 210 100 100 7 | grep died
+	./bin/philo 4 210 100 100 7 | grep die
 	sleep $time
 	x=$(( $x + 1 ))
 done
 
 x=1
 time=0.1
-echo 4 310 200 100 should die
+echo "\n4 310 200 100 should die\n"
 while [ $x -le 25 ]
 do
 	echo "Test: $x"
-	./bin/philo 4 210 100 100 7 | grep died
+	./bin/philo 4 310 200 100 7 | grep die
 	sleep $time
 	x=$(( $x + 1 ))
 done
 
 x=1
 time=0.1
-echo 5 400 100 100 not die
+echo "\n5 400 100 100 not die\n"
 while [ $x -le 25 ]
 do
 	echo "Test: $x"
-	./bin/philo 4 210 100 100 7 | grep died
+	./bin/philo 5 400 100 100 7 | grep die
 	sleep $time
 	x=$(( $x + 1 ))
 done
 
 x=1
 time=0.1
-echo 5 200 100 100 should die
+echo "\n5 200 100 100 should die\n"
 while [ $x -le 25 ]
 do
 	echo "Test: $x"
-	./bin/philo 4 210 100 100 7 | grep died
+	./bin/philo 5 200 100 100 7 | grep die
+	sleep $time
+	x=$(( $x + 1 ))
+done
+
+x=1
+time=0.1
+echo "\n3 400 100 100 not die\n"
+while [ $x -le 25 ]
+do
+	echo "Test: $x"
+	./bin/philo 3 400 100 100 7 | grep die
+	sleep $time
+	x=$(( $x + 1 ))
+done
+
+x=1
+time=0.1
+echo "\n3 200 100 100 should die\n"
+while [ $x -le 25 ]
+do
+	echo "Test: $x"
+	./bin/philo 3 200 100 100 7 | grep die
 	sleep $time
 	x=$(( $x + 1 ))
 done
