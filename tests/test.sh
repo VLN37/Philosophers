95e18be72795516dab7930bb8ee01234b836a6a7
VLN37 <54167882+VLN37@users.noreply.github.com>
2022-01-22 07:50:02 -0300

    tests: better presentation

--- tests/test.sh
+++ tests/test.sh
@@ -1,25 +1,26 @@
 x=1
 time=0.1
+BINPATH=./bin/philo
 echo "1 400 100 100 should die\n"
 while [ $x -le 2 ]
 do
 	echo "Test: $x"
-	./bin/philo 1 400 100 100 7 | grep die
+	$BINPATH 1 400 100 100 7 | grep die
 	sleep $time
 	x=$(( $x + 1 ))
 done
 
 x=1
 time=0.1
-echo "2 150 360 100 25 should die\n"
-echo "2 100 200 200 25 should die\n"
+echo "\n2 150 360 100 25 should die"
+echo "2 100 200 200 25 should die"
 echo "2 150 200 100 25 should die\n"
 while [ $x -le 2 ]
 do
 	echo "Test: $x"
-	./bin/philo 2 150 360 100 25 | grep die
-	./bin/philo 2 100 200 200 25 | grep die
-	./bin/philo 2 150 200 100 25 | grep die
+	$BINPATH 2 150 360 100 25 | grep die
+	$BINPATH 2 100 200 200 25 | grep die
+	$BINPATH 2 150 200 100 25 | grep die
 	sleep $time
 	x=$(( $x + 1 ))
 done
@@ -30,7 +31,7 @@ echo "\n4 210 100 100 not die\n"
 while [ $x -le 25 ]
 do
 	echo "Test: $x"
-	./bin/philo 4 210 100 100 7 | grep die
+	$BINPATH 4 210 100 100 7 | grep die
 	sleep $time
 	x=$(( $x + 1 ))
 done
@@ -41,7 +42,7 @@ echo "\n4 310 200 100 should die\n"
 while [ $x -le 25 ]
 do
 	echo "Test: $x"
-	./bin/philo 4 310 200 100 7 | grep die
+	$BINPATH 4 310 200 100 7 | grep die
 	sleep $time
 	x=$(( $x + 1 ))
 done
@@ -52,7 +53,7 @@ echo "\n5 400 100 100 not die\n"
 while [ $x -le 25 ]
 do
 	echo "Test: $x"
-	./bin/philo 5 400 100 100 7 | grep die
+	$BINPATH 5 400 100 100 7 | grep die
 	sleep $time
 	x=$(( $x + 1 ))
 done
@@ -63,7 +64,7 @@ echo "\n5 200 100 100 should die\n"
 while [ $x -le 25 ]
 do
 	echo "Test: $x"
-	./bin/philo 5 200 100 100 7 | grep die
+	$BINPATH 5 200 100 100 7 | grep die
 	sleep $time
 	x=$(( $x + 1 ))
 done
@@ -74,7 +75,7 @@ echo "\n3 400 100 100 not die\n"
 while [ $x -le 25 ]
 do
 	echo "Test: $x"
-	./bin/philo 3 400 100 100 7 | grep die
+	$BINPATH 3 400 100 100 7 | grep die
 	sleep $time
 	x=$(( $x + 1 ))
 done
@@ -85,7 +86,7 @@ echo "\n3 200 100 100 should die\n"
 while [ $x -le 25 ]
 do
 	echo "Test: $x"
-	./bin/philo 3 200 100 100 7 | grep die
+	$BINPATH 3 200 100 100 7 | grep die
 	sleep $time
 	x=$(( $x + 1 ))
 done
--- tests/testbonus.sh
+++ tests/testbonus.sh
@@ -1,25 +1,26 @@
 x=1
 time=0.1
+BINPATH=./bin/philo_bonus
 echo "1 400 100 100 should die\n"
 while [ $x -le 2 ]
 do
 	echo "Test: $x"
-	./bin/philo_bonus 1 400 100 100 7 | grep die
+	$BINPATH 1 400 100 100 7 | grep die
 	sleep $time
 	x=$(( $x + 1 ))
 done
 
 x=1
 time=0.1
-echo "2 150 360 100 25 should die\n"
-echo "2 100 200 200 25 should die\n"
-echo "2 150 200 100 25 should die\n"
+echo "\n2 150 360 100 should die"
+echo "2 100 200 200 should die"
+echo "2 150 200 100 should die\n"
 while [ $x -le 2 ]
 do
 	echo "Test: $x"
-	./bin/philo_bonus 2 150 360 100 25 | grep die
-	./bin/philo_bonus 2 100 200 200 25 | grep die
-	./bin/philo_bonus 2 150 200 100 25 | grep die
+	$BINPATH 2 150 360 100 25 | grep die
+	$BINPATH 2 100 200 200 25 | grep die
+	$BINPATH 2 150 200 100 25 | grep die
 	sleep $time
 	x=$(( $x + 1 ))
 done
@@ -30,7 +31,7 @@ echo "\n4 210 100 100 not die\n"
 while [ $x -le 25 ]
 do
 	echo "Test: $x"
-	./bin/philo_bonus 4 210 100 100 7 | grep die
+	$BINPATH 4 210 100 100 7 | grep die
 	sleep $time
 	x=$(( $x + 1 ))
 done
@@ -41,7 +42,7 @@ echo "\n4 310 200 100 should die\n"
 while [ $x -le 25 ]
 do
 	echo "Test: $x"
-	./bin/philo_bonus 4 310 200 100 7 | grep die
+	$BINPATH 4 310 200 100 7 | grep die
 	sleep $time
 	x=$(( $x + 1 ))
 done
@@ -52,7 +53,7 @@ echo "\n5 400 100 100 not die\n"
 while [ $x -le 25 ]
 do
 	echo "Test: $x"
-	./bin/philo_bonus 5 400 100 100 7 | grep die
+	$BINPATH 5 400 100 100 7 | grep die
 	sleep $time
 	x=$(( $x + 1 ))
 done
@@ -63,7 +64,7 @@ echo "\n5 200 100 100 should die\n"
 while [ $x -le 25 ]
 do
 	echo "Test: $x"
-	./bin/philo_bonus 5 200 100 100 7 | grep die
+	$BINPATH 5 200 100 100 7 | grep die
 	sleep $time
 	x=$(( $x + 1 ))
 done
@@ -74,7 +75,7 @@ echo "\n3 400 100 100 not die\n"
 while [ $x -le 25 ]
 do
 	echo "Test: $x"
-	./bin/philo_bonus 3 400 100 100 7 | grep die
+	$BINPATH 3 400 100 100 7 | grep die
 	sleep $time
 	x=$(( $x + 1 ))
 done
@@ -85,7 +86,7 @@ echo "\n3 200 100 100 should die\n"
 while [ $x -le 25 ]
 do
 	echo "Test: $x"
-	./bin/philo_bonus 3 200 100 100 7 | grep die
+	$BINPATH 3 200 100 100 7 | grep die
 	sleep $time
 	x=$(( $x + 1 ))
 done
